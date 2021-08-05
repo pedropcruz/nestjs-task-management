714fb2134777a8245fdbe7732acf3b341f416100
Pedro Cruz <pedropcruz@outlook.pt>
2021-08-05 11:31:24 +0100

    fix: added validations and error handling

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,10 @@
+import { IsEnum, IsString } from 'class-validator'
 import { TaskStatus } from '../task.model'
 
 export class GetTasksFilterDto {
+  @IsEnum(TaskStatus)
   status?: TaskStatus
+
+  @IsString()
   search?: string
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
 import { Task, TaskStatus } from './task.model'
 import { v4 as uuid } from 'uuid'
 import { CreateTaskDto } from './dto/create-task.dto'
@@ -47,11 +47,18 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id)
+    const found = this.tasks.find((task) => task.id === id)
+
+    if (!found) {
+      throw new NotFoundException(`Task with ID "${id}" not found`)
+    }
+
+    return found
   }
 
   deleteTaskById(id: string): void {
-    this.tasks = this.tasks.filter((task) => task.id !== id)
+    const found = this.getTaskById(id)
+    this.tasks = this.tasks.filter((task) => task.id !== found.id)
   }
 
   updateTaskStatusById(id: string, status: TaskStatus): Task {
