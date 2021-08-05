0b585a934fb3da58f22e3d7a1d938eb5f6ec9060
Pedro Cruz <pedropcruz@outlook.pt>
2021-08-05 11:32:06 +0100

    fix: added validator on query parameters status and search

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,10 +1,12 @@
-import { IsEnum, IsString } from 'class-validator'
+import { IsEnum, IsOptional, IsString } from 'class-validator'
 import { TaskStatus } from '../task.model'
 
 export class GetTasksFilterDto {
+  @IsOptional()
   @IsEnum(TaskStatus)
   status?: TaskStatus
 
+  @IsOptional()
   @IsString()
   search?: string
 }
